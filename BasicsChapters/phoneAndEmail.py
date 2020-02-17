a0d3c528038d41c5ef37365ceb40a13c82782861
amschon90 <amschon90@gmail.com>
2020-02-17 13:17:12 -0700

    Ch. 7 regular expressions project, finds phone numbers and emails

--- BasicsChapters/phoneAndEmail.py
+++ BasicsChapters/phoneAndEmail.py
@@ -7,7 +7,7 @@ phoneRegex = re.compile(r'''(
     (\d{3})                             # first 3
     (\s|-|\.)                           # separator
     (\d{4})                             # last 4
-    (\s*(ext|x|ext.)\s*(\d{2,5}))?      #extension
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?      # extension
 )''', re.VERBOSE)
 
 # TO DO, create email regex
@@ -18,6 +18,16 @@ emailRegex = re.compile(r'''(
     (\.[a-zA-Z]{2,4})                   # dot something
 )''', re.VERBOSE)
 
-# TO DO, find matches
+text = "Hello, my name is Andy Schon! My number is 330-774-2211, or you can reach me at work at (602)-453-5030." \
+       "Of course, if you're shy, email is fine too. I can be reached at Amschon90@gmail.com"
 
-# TO DO, copy results
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':                 # checks to see if theres an extension on the phone number
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+print(matches)
