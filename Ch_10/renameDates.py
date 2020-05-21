7b7d1ae48a903a3118365d2a8ee4aee881c7bea7
amschon90 <amschon90@gmail.com>
2020-05-21 13:52:00 -0700

    File renamer project built to convert US date log files to Euro date formatting.

--- Ch_10/renameDates.py
+++ Ch_10/renameDates.py
@@ -1,15 +1,41 @@
 # Renames filenames with American date format to European.
 
+import shutil, os, re
+
+myDir = 'C:\\Users\\aschon1\\ATBS Junk Files\\Proj_Ch10_JunkDir'
+myFiles = os.listdir(myDir)
+
 # Create a regex that matches files with American date format
+datePattern = re.compile(r"""^(.*?) # all text before the date
+       ((0|1)?\d)-                     # one or two digits for the month
+       ((0|1|2|3)?\d)-                 # one or two digits for the day
+       ((19|20)\d\d)                   # four digits for the year
+       (.*?)$                          # all text after the date
+       """, re.VERBOSE)
 
 # Loop over the files in the working directory
+for amerFilename in myFiles:
+    mo = datePattern.search(amerFilename)
 
-# Skip files without a date
+    # Skip files without a date
+    if mo == None:
+        continue
 
-# Get the different parts of the filename
+    # Get the different parts of the filename
+    beforePart = mo.group(1)
+    monthPart = mo.group(2)
+    dayPart = mo.group(4)
+    yearPart = mo.group(6)
+    afterPart = mo.group(8)
 
-# Form the European style name
+    # Form the European style name
+    euroFilename = beforePart+dayPart+'-'+monthPart+'-'+yearPart+afterPart
 
-# Get the full, absolute file paths
+    # Get the full, absolute file paths
+    absWorkingDir = os.path.abspath('.')
+    amerFilename = os.path.join(myDir, amerFilename)
+    euroFilename = os.path.join(myDir, euroFilename)
 
-# Rename the files
+    # Rename the files
+    print(f'Renaming "{amerFilename}" to "{euroFilename}"...')
+    shutil.move(amerFilename, euroFilename)
