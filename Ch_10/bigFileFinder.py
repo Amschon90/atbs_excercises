9f86efeb1ed11b9a5f539fc1a536acde2e0449b8
amschon90 <amschon90@gmail.com>
2020-05-22 13:53:53 -0700

    Finalized Big File Finder script.

---
+++ Ch_10/bigFileFinder.py
@@ -0,0 +1,20 @@
+# Script to find giant files in my documents
+
+import shutil, os
+
+folder = ('C:\\Users\\aschon1\\Documents')
+
+def fileSizeSearch(folder):
+    potentialfreedmemory = 0
+    filecounter = 0
+    print('Searching for pictures..')
+    for foldername, subfolders, filenames in os.walk(folder):
+        for filename in filenames:
+            filesize = os.path.getsize(os.path.join(folder, foldername, filename))
+            if filesize >= 100000000:
+                potentialfreedmemory += filesize
+                filecounter += 1
+                print(f'{filename} has a size of {int(filesize/1000000)}MB and should be deleted or archived.')
+    print(f"\n\nBy deleting these {filecounter} files, you could free up {int(filesize/1000000)}MB of memory.")
+
+fileSizeSearch(folder)
