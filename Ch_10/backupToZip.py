61475e72688b4bba9304933ff35e94ad6d2549fe
amschon90 <amschon90@gmail.com>
2020-05-22 11:35:59 -0700

    Finalized file backup script.

--- Ch_10/backupToZip.py
+++ Ch_10/backupToZip.py
@@ -10,16 +10,27 @@ def backupToZip(folder):
     fileNumber = 1
 
     while True:
-        zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        zipFilename = os.path.basename(folder) + '_' + str(fileNumber) + '.zip'
         if not os.path.exists(zipFilename):
             break
         fileNumber += 1
 
-    # TODO: Create the zip file
+    # Create the zip file
+    print(f'Creating {zipFilename}...')
+    backupZip = zipfile.ZipFile(zipFilename,'w')
 
-    # TODO: Walk the entire folder tree and compress folder.
+    # Walk the entire folder tree and compress folder.
+    for foldername, subfolders, filenames in os.walk(folder):
+        print(f'Adding files in {foldername}')
+        backupZip.write(foldername)
 
+        # Add all the files in this folder to the ZIP file.
+        for filename in filenames:
+            newBase = os.path.basename(folder) + '_'
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue  # don't back up the backup ZIP files
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
     print('Done.')
-
 
 backupToZip('C:\\Users\\aschon1\\ATBS Junk Files\\Proj_Ch10_JunkDir\\bu')
