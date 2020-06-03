a78c46a8ad527987d946cf3370561762062d795d
amschon90 <amschon90@gmail.com>
2020-06-03 13:00:17 -0700

    Simple script to test a website connection and download it.

--- Ch_12/fileDownloader.py
+++ Ch_12/fileDownloader.py
@@ -1,24 +1,30 @@
+# You can learn about the requests module’s other features from https://requests.readthedocs.org/
 import requests
 
+# call requests to download the file
 res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+# this is a fake file for testing when needed
 fakeres = requests.get('https://inventwithpython.com/page_that_does_not_exist')
 
 menuOption = int(input('Which of the following would you like to do?\n1) Inspect the text\n2) Download the text\n'))
 
 if menuOption == 1:
     print(type(res))
+    # this checks if it was successful
     print(res.status_code == 200)
     print(len(res.text))
-    print(res.text[0:10])
+    print(res.text[0:250])
     print("\n")
-
-    try:
+    try:     # put this try block in so it won't crash
         fakeres.raise_for_status()
-    except Exception as exc:
+    except Exception as exc:     # throws exception
         print('Oh no! It broke...\n%s' % (exc))
 
 if menuOption == 2:
     res.raise_for_status()
+    # creates file with 'wb' write binary mode
     playFile = open('RomeoAndJuliet.txt', 'wb')
+    # takes 100k bytes at a time
     for chunk in res.iter_content(100000):
         playFile.write(chunk)
+    playFile.close()
