69e71e2dc7515b97043a58354573c78dc80ec9ac
Andy Schon <amschon90@gmail.com>
2020-07-29 21:33:50 -0700

    finalized pdf combiner excercise

---
+++ Ch_14/pdfCombiner.py
@@ -0,0 +1,32 @@
+ # Combines all the PDFs in the current working directory into one PDF.
+
+import PyPDF2, os
+
+os.chdir('C:\\Users\\Zig0n\\Documents\\VSCode Projects')
+
+pdfFiles = []
+
+# Get the file names.
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+
+# Put the docs in order
+pdfFiles.sort(key = str.lower)
+
+pdfWriter = PyPDF2.PdfFileWriter()
+
+# Loop through all the PDF files.
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+# Loop through all the pages (except the first) and add them.
+for pageNum in range(1, pdfReader.numPages):
+    pageObj = pdfReader.getPage(pageNum)
+    pdfWriter.addPage(pageObj)
+
+# Save the resulting PDF to a file.
+pdfOutput = open('allminutes.pdf','wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
