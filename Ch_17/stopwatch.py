b49acbe3e67b076a38b6d4816aa2018b504da40f
Andy Schon <amschon90@gmail.com>
2020-08-14 12:30:35 -0700

    finalized stopwatch improvements

--- Ch_17/stopwatch.py
+++ Ch_17/stopwatch.py
@@ -1,23 +1,31 @@
 #! python3
 # stopwatch.py - A simple stopwatch program.
 
-import time
+import time, pyperclip
 
-# Display instructions
-print('Press ENTER to begin. Afterward, press ENTER to click the stopwatch. Press CTRL+C to quit.')
-input()
-print('Started.')
-startTime = time.time()
-lastTime = startTime
-lapNum = 1
+def stopwatch():
+    # Display instructions
+    print('Press ENTER to begin. Afterward, press ENTER to click the stopwatch. Press CTRL+C to quit.')
+    input()
+    print('Started.')
+    startTime = time.time()
+    lastTime = startTime
+    lapNum = 1
+    output = ''
 
-try:
-    while True:
-        input()
-        lapTime = round(time.time()-lastTime,2)
-        totalTime = round(time.time()-startTime, 2)
-        print('Lap #%s: %s (Total: %s)' % (lapNum, lapTime, totalTime), end='')
-        lapNum += 1
-        lastTime = time.time()
-except KeyboardInterrupt:
-    print('\nDone')
+    try:
+        while True:
+            input()
+            lapTime = round(time.time()-lastTime,2)
+            totalTime = round(time.time()-startTime, 2)
+            lapOutput = (f'Lap #{str(lapNum).rjust(2)}: {str(lapTime).ljust(5)} (Total: {str(totalTime).ljust(5)})')
+            output += lapOutput + '\n'
+            print(lapOutput, end='')
+            lapNum += 1
+            lastTime = time.time()
+    except KeyboardInterrupt:
+        pyperclip.copy(str(output))
+        print('\nDone')
+
+if __name__ == '__main__':
+    stopwatch()
