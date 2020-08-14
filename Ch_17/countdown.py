b1136e3e9bcb4f59ad972cc123a3d987920f0950
Andy Schon <amschon90@gmail.com>
2020-08-14 11:59:28 -0700

    tweaks to countdown program

--- Ch_17/countdown.py
+++ Ch_17/countdown.py
@@ -1,10 +1,15 @@
 import subprocess, time
 
-timeLeft = 3
+timeLeft = 60
 
-while timeLeft > 0:
-    print(timeLeft, end='')
-    timeLeft -= 1
-    time.sleep(1)
+print('Starting countdown. Press CTRL+C to end early.')
+try:
+    while timeLeft > 0:
+        print(timeLeft)
+        timeLeft -= 1
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print('Done.')
 
 subprocess.Popen(['start', 'alarm.wav'], shell=True)
