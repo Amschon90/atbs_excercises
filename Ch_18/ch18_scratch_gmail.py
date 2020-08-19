ce62fa1483abe57668b7840e1b32a59a39b87614
Andy Schon <amschon90@gmail.com>
2020-08-19 13:47:39 -0700

    Ch. 18 excercises pt 1

---
+++ Ch_18/ch18_scratch_gmail.py
@@ -0,0 +1,6 @@
+import ezgmail, os
+import gmail_credentials
+#os.chdir(r'C:\Users\Zig0n\Documents\VSCode Projects\atbs_excercises\Ch_18')
+#ezgmail.init()
+
+ezgmail.send('Kronozftw@gmail.com', 'Hey!', "I'm sending you this via Python.")
---
+++ Ch_18/ch18_smtp.py
@@ -0,0 +1,18 @@
+import smtplib
+import gmail_credentials
+
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+
+# if 250 returns, that means success
+smtpObj.ehlo()
+
+# if 220 returns, that means ready
+smtpObj.starttls()
+
+smtpObj.login(gmail_credentials.username,gmail_credentials.password)
+
+smtpObj.sendmail(f'{gmail_credentials.username}', 'kronozftw@gmail.com', 'Subject: Hello!\nHello World.')
+# Returns dictionary of failures
+
+# Disconnect from server, 221 means ending
+smtpObj.quit()
